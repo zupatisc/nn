f1c7bc76eff5a8cdbd08f6b227ada6da9c24d91f
Tischmann, Patrik (zupatisc) <patrik.tischmann@stud.hs-ruhrwest.de>
2022-12-06 23:23:47 +0100

    Make functions for testing different functionality
    Not as good as actual tests but it might be a good in-between for now.

--- src/main.c
+++ src/main.c
@@ -2,12 +2,9 @@
 #include <stdio.h>
 
 #include "tensor.h"
+#include "layer_dense.h"
 
-
-int main() {
-
-    puts("Sanity test");
-
+int test_tensors() {
     /*
      * Very basic tensor test
      */
@@ -41,17 +38,53 @@ int main() {
     second_tensor->matrix[2][0] = 9;
     second_tensor->matrix[2][1] = 10;
 
-    tensor_matmul(target_tensor, first_tensor, second_tensor);
+    tensor_dot(target_tensor, first_tensor, second_tensor);
     tensor_print(target_tensor);
 
     printf("tensor_iter Test: %f\n", tensor_iter(target_tensor, 3));
 
+    puts("\n");
     Tensor *rnd_tensor = tensor_rinit(2, 3);
     tensor_print(rnd_tensor);
 
     tensor_destroy(first_tensor);
     tensor_destroy(second_tensor);
     tensor_destroy(target_tensor);
+    tensor_destroy(rnd_tensor);
+
+
+    printf("Tensor Test success!\n");
+    return EXIT_SUCCESS;
+}
+
+int test_layer_dense() {
+
+    Layer_Dense *test_layer = layer_dense_init(1, 8);
+    tensor_print(test_layer->weights);
+    tensor_print(test_layer->biases);
+
+    Tensor *test_tensor = tensor_init(4, 1, 1);
+    tensor_print(test_tensor);
+
+    /* if(tensor_dot(test_layer->output, test_tensor, test_layer->weights))
+        printf("EXIT FAILURE");
+    tensor_print(test_layer->output); */
+    layer_dense_forward(test_layer, test_tensor);
+
+    layer_dense_destroy(test_layer);
+    tensor_destroy(test_tensor);
+
+    printf("Layer Test success!\n");
+    return EXIT_SUCCESS;
+}
+
+int main() {
+
+    puts("Sanity test");
+
+    // test_tensors();
+    test_layer_dense();
+
 
     return EXIT_SUCCESS;
 }
